93c2c12eb6638fd050620e3abd23bebca529281e
Chris Colborne <zorfling@gmail.com>
2015-06-15 08:59:19 +1000

    Add location test

--- app/src/main/java/com/zorfling/sunshine/data/WeatherContract.java
+++ app/src/main/java/com/zorfling/sunshine/data/WeatherContract.java
@@ -41,6 +41,17 @@ public class WeatherContract {
     public static final class LocationEntry implements BaseColumns {
         public static final String TABLE_NAME = "location";
 
+        // Location Query
+        public static final String COLUMN_LOCATION_SETTING = "location_setting";
+
+        // City Name
+        public static final String COLUMN_CITY_NAME = "city_name";
+
+        // Latitude
+        public static final String COLUMN_COORD_LAT = "coord_lat";
+
+        // Longitude
+        public static final String COLUMN_COORD_LONG = "coord_long";
     }
 
     /* Inner class that defines the table contents of the weather table */
--- app/src/main/java/com/zorfling/sunshine/data/WeatherDbHelper.java
+++ app/src/main/java/com/zorfling/sunshine/data/WeatherDbHelper.java
@@ -70,6 +70,22 @@ public class WeatherDbHelper extends SQLiteOpenHelper {
                 WeatherEntry.COLUMN_LOC_KEY + ") ON CONFLICT REPLACE);";
 
         sqLiteDatabase.execSQL(SQL_CREATE_WEATHER_TABLE);
+
+        final String SQL_CREATE_LOCATION_TABLE = "CREATE TABLE " + LocationEntry.TABLE_NAME + " (" +
+                // Why AutoIncrement here, and not above?
+                // Unique keys will be auto-generated in either case.  But for weather
+                // forecasting, it's reasonable to assume the user will want information
+                // for a certain date and all dates *following*, so the forecast data
+                // should be sorted accordingly.
+                LocationEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+
+                // the ID of the location entry associated with this weather data
+                LocationEntry.COLUMN_CITY_NAME + " TEXT NOT NULL, " +
+                LocationEntry.COLUMN_LOCATION_SETTING + " TEXT UNIQUE NOT NULL, " +
+                LocationEntry.COLUMN_COORD_LAT + " REAL NOT NULL, " +
+                LocationEntry.COLUMN_COORD_LONG + " REAL NOT NULL);";
+
+        sqLiteDatabase.execSQL(SQL_CREATE_LOCATION_TABLE);
     }
 
     @Override
